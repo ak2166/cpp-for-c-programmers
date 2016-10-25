af14f0e3125aca204b4aa6ec2a37d1dc7cb049f8
Anthony A. Kolodzinski <anthonykolodzinski@gmail.com>
2016-10-25 08:54:33 -0400

    First commit. Complete assignment 1 of converting c program to c++.

---
+++ include/c_conversion.h
@@ -0,0 +1,10 @@
+//
+// Created by Anthony Kolodzinski on 10/25/16.
+//
+
+#ifndef CPP_FOR_C_PROGRAMMERS_ASSIGNMENTS_C_CONVERSION_H
+#define CPP_FOR_C_PROGRAMMERS_ASSIGNMENTS_C_CONVERSION_H
+
+void sum(int& p, int n, vector<int> d);
+
+#endif //CPP_FOR_C_PROGRAMMERS_ASSIGNMENTS_C_CONVERSION_H
